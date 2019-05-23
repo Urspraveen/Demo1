1d0d63b23288ab47a08e695b8e2e129d5ebdea9d
praveenkumar <praveen224sharma@gmail.com>
2019-05-24 01:34:33 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+var firstmessage = "hello world";
+console.log(firstmessage);
+var multiply = function (a, b) {
+    return a * b;
+};
+console.log(multiply(2, 3));
---
+++ main.ts
@@ -0,0 +1,9 @@
+let firstmessage="hello world";
+console.log(firstmessage);
+
+let multiply=(a:number,b:number)=>{
+    return a*b;
+}
+
+
+console.log(multiply(2,3));
